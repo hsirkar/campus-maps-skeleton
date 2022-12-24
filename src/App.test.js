a759cf189401205964ef2cb979d10410955e54dd
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2022-12-23 19:23:57 -0500

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const root = ReactDOM.createRoot(div);
+    root.render(<App />);
 });
