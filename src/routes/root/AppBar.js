0e8e2115f42962004cfee7b775d7f65752283d3f
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-01-03 23:56:26 -0500

    Remove search bar popover

--- src/routes/root/AppBar.js
+++ src/routes/root/AppBar.js
@@ -7,144 +7,44 @@ import {
     Box,
     Divider,
     Fade,
-    Grid,
     IconButton,
     InputBase,
     LinearProgress,
-    List,
-    ListItemButton,
     Menu,
     MenuItem,
     Paper,
-    Popover,
     Toolbar,
     Typography,
 } from '@mui/material';
 import { useNavigation } from 'react-router';
 
-import Chip from '../../common/Chip';
-import types from '../../postTypes';
-
 export function CustomizedInputBase() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const handlePopoverOpen = event => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
-    const open = Boolean(anchorEl);
-
     return (
-        <React.Fragment>
-            <Paper
-                component="form"
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    width: 600,
-                    boxShadow: 0,
-                    borderRadius: 2,
-                    borderBottomLeftRadius: 0,
-                    borderBottomRightRadius: 0,
-                    '&:focus-within': { boxShadow: 1 },
-                    transition: 'none',
-                    boxSizing: 'border-box',
-                }}>
-                <IconButton type="button" sx={{ pl: 2 }} aria-label="search">
-                    <Search />
-                </IconButton>
-                <InputBase
-                    onFocus={e => handlePopoverOpen(e)}
-                    onBlur={e => handlePopoverClose(e)}
-                    sx={{ ml: 1, flex: 1 }}
-                    placeholder="Search University of Maryland"
-                />
-                <IconButton type="button" sx={{ pl: 2 }} aria-label="search">
-                    <Close />
-                </IconButton>
-            </Paper>
-            <Popover
-                open={open}
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: -4,
-                    horizontal: 'center',
-                }}
-                onClose={handlePopoverClose}
-                disableRestoreFocus
-                disableAutoFocus
-                disableEnforceFocus
-                sx={{ marginLeft: -0.5 }}
-                PaperProps={{ elevation: 1, left: -5 }}>
-                <Box sx={{ px: 4, py: 2, width: 600, boxSizing: 'border-box' }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <Typography variant="h4">Recent</Typography>
-                            <List dense>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                            </List>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography variant="h4">Trending</Typography>
-                            <List dense>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                                <ListItemButton>
-                                    asdfasdfasdfasdf
-                                </ListItemButton>
-                            </List>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography variant="h4" mb={1.5}>
-                                Explore Events
-                            </Typography>
-                            {types.events.map(e => (
-                                <Chip
-                                    key={e.url}
-                                    type="events"
-                                    subtype={e.url}
-                                />
-                            ))}
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography variant="h4" mb={1.5}>
-                                Explore Places
-                            </Typography>
-                            {types.places.map(p => (
-                                <Chip
-                                    key={p.url}
-                                    type="places"
-                                    subtype={p.url}
-                                />
-                            ))}
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Popover>
-        </React.Fragment>
+        <Paper
+            component="form"
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                width: 600,
+                boxShadow: 0,
+                borderRadius: 2,
+                borderBottomLeftRadius: 0,
+                borderBottomRightRadius: 0,
+                '&:focus-within': { boxShadow: 1 },
+                transition: 'none',
+                boxSizing: 'border-box',
+            }}>
+            <IconButton type="button" sx={{ pl: 2 }} aria-label="search">
+                <Search />
+            </IconButton>
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search University of Maryland"
+            />
+            <IconButton type="button" sx={{ pl: 2 }} aria-label="search">
+                <Close />
+            </IconButton>
+        </Paper>
     );
 }
 
