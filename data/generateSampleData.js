8b6bb94e69a56483163f8f24580377d5b9af8ae5
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-01-05 18:07:33 -0500

    Generate sample auth

--- data/generateSampleData.js
+++ data/generateSampleData.js
@@ -1,245 +1,63 @@
-const { uid, randEl, irange, range, randomDate, db } = require('./util');
-
 const { LoremIpsum } = require('lorem-ipsum');
 const lorem = new LoremIpsum();
-
 const {
     Timestamp,
     GeoPoint,
     writeBatch,
     doc,
 } = require('firebase/firestore/lite');
-
+const { createUserWithEmailAndPassword } = require('firebase/auth');
+const fs = require('fs/promises');
+const {
+    uid,
+    randEl,
+    irange,
+    range,
+    randomDate,
+    db,
+    auth,
+    adjectives,
+    nouns,
+} = require('./util');
 const types = require('../src/postTypes.js');
+
 const eventTypes = types.events.map(e => e.url);
 const placeTypes = types.places.map(p => p.url);
 
-const adjectives = [
-    'shapeless',
-    'assured',
-    'dazzling',
-    'unconscious',
-    'temporary',
-    'fellow',
-    'impossible',
-    'specialized',
-    'puzzled',
-    'gradual',
-    'dusty',
-    'exhausted',
-    'countless',
-    'daring',
-    'delightful',
-    'drastic',
-    'waiting',
-    'intent',
-    'shadowy',
-    'battered',
-    'naval',
-    'educated',
-    'urban',
-    'pro',
-    'ample',
-    'distinguished',
-    'wooden',
-    'following',
-    'positive',
-    'eligible',
-    'organic',
-    'formal',
-    'sturdy',
-    'welcome',
-    'rubber',
-    'accessible',
-    'glorious',
-    'complete',
-    'previous',
-    'cautious',
-    'dominant',
-    'rational',
-    'lousy',
-    'regulatory',
-    'feminine',
-    'cardboard',
-    'grave',
-    'misleading',
-    'rocky',
-    'overseas',
-    'accustomed',
-    'scattered',
-    'magnificent',
-    'honorable',
-    'known',
-    'sudden',
-    'male',
-    'doctoral',
-    'overwhelming',
-    'full-time',
-    'architectural',
-    'resourceful',
-    'embarrassed',
-    'minuscule',
-    'direct',
-    'curved',
-    'controlling',
-    'utopian',
-    'disorderly',
-    'merciful',
-    'corporate',
-    'heartening',
-    'foolish',
-    'untenable',
-    'junior',
-    'primitive',
-    'deserted',
-    'collected',
-    'disastrous',
-    'never-ending',
-    'calm',
-    'fetid',
-    'appropriate',
-    'unfair',
-    'wise',
-    'precious',
-    'shut',
-    'venomous',
-    'unaware',
-    'obsessed',
-    'muddy',
-    'brave',
-    'illuminated',
-    'two-bit',
-    'noisy',
-    'lost',
-    'independent',
-    'compact',
-    'stored',
-    'hazel',
-];
-
-const nouns = [
-    'slick',
-    'glitter',
-    'puddle',
-    'quake',
-    'pearl',
-    'handgun',
-    'cleavage',
-    'blindness',
-    'serving',
-    'snarl',
-    'solace',
-    'bedding',
-    'reprisal',
-    'prohibition',
-    'entrepreneur',
-    'goblin',
-    'fission',
-    'protagonist',
-    'noun',
-    'allusion',
-    'cookbook',
-    'urn',
-    'venom',
-    'esteem',
-    'gland',
-    'hulk',
-    'questionnaire',
-    'fiasco',
-    'wrench',
-    'spasm',
-    'tack',
-    'receipt',
-    'jargon',
-    'rapture',
-    'heading',
-    'backup',
-    'hiss',
-    'flutist',
-    'freeway',
-    'liner',
-    'chord',
-    'spiral',
-    'crate',
-    'healer',
-    'seduction',
-    'expressway',
-    'fad',
-    'lark',
-    'mosque',
-    'windmill',
-    'carnival',
-    'billboard',
-    'coral',
-    'future',
-    'clump',
-    'make-up',
-    'extent',
-    'leash',
-    'stunt',
-    'recreation',
-    'proximity',
-    'pal',
-    'link',
-    'imperialism',
-    'documentary',
-    'gutter',
-    'porter',
-    'slogan',
-    'reminder',
-    'boardwalk',
-    'paradigm',
-    'accountant',
-    'psychologist',
-    'republic',
-    'shudder',
-    'waitress',
-    'cellar',
-    'radar',
-    'superpower',
-    'hood',
-    'liar',
-    'amateur',
-    'jeopardy',
-    'terminal',
-    'cure',
-    'thorn',
-    'heartland',
-    'transport',
-    'candy',
-    'son-in-law',
-    'graduation',
-    'pasture',
-    'blur',
-    'sting',
-    'stairwell',
-    'agenda',
-    'sanctuary',
-    'pigment',
-    'orthodoxy',
-    'indictment',
-];
-
-function generateSampleData(numUsers, numPosts) {
-    // Generate users
+async function main(numUsers, numPosts) {
+    // Generate sample users
+    console.log('Generating sample users...');
     const users = [];
 
     for (let i = 0; i < numUsers; i++) {
         const username = randEl(adjectives) + '-' + randEl(nouns);
+        const isStudent = Math.random() < 0.5;
+        const campus = 'umd';
+        const email = `${username}@${
+            isStudent
+                ? randEl(['umd.edu', 'terpmail.umd.edu'])
+                : randEl(['gmail.com', 'hotmail.com', 'yahoo.com'])
+        }`;
+        const password = 'password';
+        const profilePic = `https://source.boringavatars.com/beam/120/${username}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`;
+
+        const { user } = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
         users.push({
-            id: uid(),
-            username: username,
-            profilePic: `https://source.boringavatars.com/beam/120/${username}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`,
-            followers: [],
-            following: [],
-            saved: [],
-            isStudent: true,
-            email: username + '@umd.edu',
-            campus: 'umd',
+            id: user.uid,
+            username,
+            isStudent,
+            campus,
+            email,
+            profilePic,
         });
     }
 
-    // Generate posts
+    // Generate sample posts
+    console.log('Generating sample posts...');
     const posts = [];
 
     for (let i = 0; i < numPosts; i++) {
@@ -295,34 +113,8 @@ function generateSampleData(numUsers, numPosts) {
         });
     }
 
-    // Generate followers, following, and saved
-    for (let i = 0; i < numUsers; i++) {
-        const followers = [
-            ...new Set(
-                Array(irange(0, numUsers))
-                    .fill(0)
-                    .map(() => randEl(users).id)
-            ),
-        ].filter(f => f !== users[i].id);
-        users[i].followers = followers;
-
-        for (let j = 0; j < followers.length; j++) {
-            users.find(u => u.id === followers[j]).following.push(users[i].id);
-        }
-
-        users[i].saved = [
-            ...new Set(
-                Array(irange(0, numPosts / 2))
-                    .fill(0)
-                    .map(() => randEl(posts).id)
-            ),
-        ];
-    }
-
-    return { posts, users };
-}
-
-async function uploadSampleData({ posts, users }) {
+    // Write to firestore
+    console.log('Writing to firestore...');
     const batch = writeBatch(db);
 
     for (const post of posts) {
@@ -336,12 +128,12 @@ async function uploadSampleData({ posts, users }) {
     }
 
     await batch.commit();
+    await fs.writeFile(
+        'generated.json',
+        JSON.stringify({ posts, users }, null, 2)
+    );
+
+    console.log('Done!');
 }
 
-console.log('generating...');
-
-const data = generateSampleData(25, 300);
-
-console.log('uploading...');
-
-uploadSampleData(data).then(() => console.log('done!'));
+main(25, 300);
